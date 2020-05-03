ebc68665acf9d1a51695c36f92192c239058cc8e
Richard Hanolt <rhanolt@gmail.com>
2020-05-03 22:36:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,5 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
-  assign_rooms(attendees)
+  attendees.each{|attendees| batch_badge_creator(attendees) assign_rooms(attendees)}
 end
